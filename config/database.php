85c5a0b0fe0a8cab96bca66d4eda4a1b0662f2a3
Armando Sandino <jonwinlive@gmail.com>
2020-04-26 13:16:53 -0600

    reconfigurando fichero database.php

--- config/database.php
+++ config/database.php
@@ -43,20 +43,22 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 /**
- *             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-*/
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+
+
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'contactodb'),
             'username' => env('DB_USERNAME', 'user_contactodb'),
             'password' => env('DB_PASSWORD', 'sandino'),
+*/
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
 
             'charset' => 'utf8',
