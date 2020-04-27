087198da35daa03442e2e3313ca9588007552780
Armando Sandino <jonwinlive@gmail.com>
2020-04-26 21:00:49 -0600

    ruta insertar usuario

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::ApiResource('contactos','ContactoController');
+Route::group(['middleware'=>'auth:api'], function () {
+    //Route::ApiResource('contactos','ContactoController');
+});
+
+Route::ApiResource('usuario','UserController');
+
+Route::group( ['middleware'=>'auth:api'], function() {
+    //Route::ApiResource('usuario','UserController');
+});
