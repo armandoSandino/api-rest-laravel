2090cdf3b84ce3ccbe81d019834e23dbf96684a5
Armando Sandino <jonwinlive@gmail.com>
2020-04-26 22:31:56 -0600

    metodo insertar controlador usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,16 @@ use App\User;//modelo user
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     public function store( Request $request ) {
+
         $input = $request->all();
+
+        $input['password'] = Hash::make( $request->password );
+
         User::create( $input );
         return response()->json([
             'res'=> true,
