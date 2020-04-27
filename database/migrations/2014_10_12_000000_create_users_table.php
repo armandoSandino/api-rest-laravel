69ba037a94cc941f2780815914b59a6cab4c77be
Armando Sandino <jonwinlive@gmail.com>
2020-04-26 21:01:15 -0600

    modificando tabla usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
             $table->rememberToken();
+
+            $table->string('api_token')->nullable();
+            
             $table->timestamps();
         });
     }
