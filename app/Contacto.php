4447b8fbd2aef2aa6e6c761662b7d27206a25028
Armando Sandino <jonwinlive@gmail.com>
2020-04-27 00:12:34 -0600

    modificando modelo contacto

--- app/Contacto.php
+++ app/Contacto.php
@@ -7,10 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Contacto extends Model
 {
 protected $table = "contacto";
-protected $fillable = [
-    'nombre',
-    'direccion',
-    'telefono',
-    'foto'
-];
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'telefono',
+        'foto'
+    ];
+        protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public static function rules($isNew = true)
+    {
+        return [
+            'nombre' => 'required|min:5|max:100',
+            'telefono' => 'required|unique:contacto,telefono,' . ($isNew ? '' : request('contacto')->id)
+        ];
+    }
+
 }
